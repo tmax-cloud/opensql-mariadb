a192e4592d3121bda5fd74d3f61a16c80ffe1eaf
Jan Lindström <jan.lindstrom@mariadb.com>
2019-06-12 16:43:31 +0300

    Fix compiler error on Windows.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -5794,6 +5794,7 @@ int mysqld_main(int argc, char **argv)
   if (Events::init((THD*) 0, opt_noacl || opt_bootstrap))
     unireg_abort(1);
 
+#ifdef WITH_WSREP
   if (WSREP_ON)
   {
     wsrep_init_globals();
@@ -5812,6 +5813,7 @@ int mysqld_main(int argc, char **argv)
       wsrep_create_appliers(wsrep_slave_threads - 1);
     }
   }
+#endif /* WITH_WSREP */
 
   if (opt_bootstrap)
   {
