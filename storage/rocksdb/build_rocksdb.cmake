ib-mlatin/server

Change Find*.cmake modules to match conventions

storage/rocksdb/build_rocksdb.cmake
@@ -35,48 +35,48 @@ endif()
 # Optional compression libraries.
 
 include(CheckFunctionExists)
-macro(check_lib package var)
-  STRING(TOUPPER ${package} PACKAGE_NAME)
+macro(check_lib package)
   SET(WITH_ROCKSDB_${package} AUTO CACHE STRING
         "Build RocksDB  with ${package} compression. Possible values are 'ON', 'OFF', 'AUTO' and default is 'AUTO'")
 
+  STRING(TOUPPER ${package} var)
   IF (NOT ${WITH_ROCKSDB_${package}} STREQUAL "OFF")
     FIND_PACKAGE(${package} QUIET)
-    SET(HAVE_ROCKSDB_${PACKAGE_NAME} TRUE)
-    IF (${${PACKAGE_NAME}_FOUND})
+    SET(HAVE_ROCKSDB_${package} TRUE)
+    IF (${${package}_FOUND})
       IF(${ARGC} GREATER 2)
         SET(CMAKE_REQUIRED_LIBRARIES ${${var}_LIBRARIES})
-        CHECK_FUNCTION_EXISTS(${ARGV2} ${var}_VALID)
+        CHECK_FUNCTION_EXISTS(${ARGV2} ${package}_VALID)
         UNSET(CMAKE_REQUIRED_LIBRARIES)
       ELSE()
-        SET(${var}_VALID TRUE)
+        SET(${package}_VALID TRUE)
       ENDIF()
     ENDIF()
   ENDIF()
-  ADD_FEATURE_INFO(ROCKSDB_${PACKAGE_NAME} HAVE_ROCKSDB_${PACKAGE_NAME} "${package} Compression in the RocksDB storage engine")
+  ADD_FEATURE_INFO(ROCKSDB_${package} HAVE_ROCKSDB_${package} "${package} Compression in the RocksDB storage engine")
 
-  IF(${${var}_VALID})
-    MESSAGE_ONCE(rocksdb_${var} "Found ${package}: ${${var}_LIBRARIES}")
-    add_definitions(-D${PACKAGE_NAME})
+  IF(${${package}_VALID})
+    MESSAGE_ONCE(rocksdb_${package} "Found ${package}: ${${var}_LIBRARIES}")
+    add_definitions(-D${var})
     include_directories(${${var}_INCLUDE_DIR})
     list(APPEND THIRDPARTY_LIBS ${${var}_LIBRARIES})
-  ELSEIF(${${PACKAGE_NAME}_FOUND})
-    MESSAGE_ONCE(rocksdb_${var} "Found unusable ${package}: ${${var}_LIBRARIES} [${ARGV2}]")
+  ELSEIF(${${package}_FOUND})
+    MESSAGE_ONCE(rocksdb_${package} "Found unusable ${package}: ${${var}_LIBRARIES} [${ARGV2}]")
   ELSE()
-    MESSAGE_ONCE(rocksdb_${var} "Could NOT find ${package}")
+    MESSAGE_ONCE(rocksdb_${package} "Could NOT find ${package}")
   ENDIF()
 
-  IF (${WITH_ROCKSDB_${package}} STREQUAL "ON"  AND NOT ${${PACKAGE_NAME}_FOUND})
+  IF (${WITH_ROCKSDB_${package}} STREQUAL "ON"  AND NOT ${${package}_FOUND})
     MESSAGE(FATAL_ERROR
       "${package} library was not found, but WITH_ROCKSDB_${package} option is ON.\
       Either set WITH_ROCKSDB_${package} to OFF, or make sure ${package} is installed")
   endif()
 endmacro()
 
-check_lib(LZ4    LZ4)
-check_lib(BZip2  BZIP2)
-check_lib(snappy snappy) # rocksdb/cmake/modules/Findsnappy.cmake violates the convention
-check_lib(ZSTD   ZSTD ZDICT_trainFromBuffer)
+check_lib(LZ4)
+check_lib(BZip2)
+check_lib(Snappy)
+check_lib(ZSTD ZDICT_trainFromBuffer)
 
 add_definitions(-DZLIB)
 list(APPEND THIRDPARTY_LIBS ${ZLIB_LIBRARY})

tmax-cloud/opensql-mariadb

storage/rocksdb/build_rocksdb.cmake
@@ -35,48 +35,48 @@ endif()
 # Optional compression libraries.
 
 include(CheckFunctionExists)
-macro(check_lib package var)
-  STRING(TOUPPER ${package} PACKAGE_NAME)
+macro(check_lib package)
   SET(WITH_ROCKSDB_${package} AUTO CACHE STRING
         "Build RocksDB  with ${package} compression. Possible values are 'ON', 'OFF', 'AUTO' and default is 'AUTO'")
 
+  STRING(TOUPPER ${package} var)
   IF (NOT ${WITH_ROCKSDB_${package}} STREQUAL "OFF")
     FIND_PACKAGE(${package} QUIET)
-    SET(HAVE_ROCKSDB_${PACKAGE_NAME} TRUE)
-    IF (${${PACKAGE_NAME}_FOUND})
-      IF(${ARGC} GREATER 2)
+    SET(HAVE_ROCKSDB_${package} TRUE)
+    IF (${${package}_FOUND})
+      IF(${ARGC} GREATER 1)
         SET(CMAKE_REQUIRED_LIBRARIES ${${var}_LIBRARIES})
-        CHECK_FUNCTION_EXISTS(${ARGV2} ${var}_VALID)
+        CHECK_FUNCTION_EXISTS(${ARGV1} ${package}_VALID)
         UNSET(CMAKE_REQUIRED_LIBRARIES)
       ELSE()
-        SET(${var}_VALID TRUE)
+        SET(${package}_VALID TRUE)
       ENDIF()
     ENDIF()
   ENDIF()
-  ADD_FEATURE_INFO(ROCKSDB_${PACKAGE_NAME} HAVE_ROCKSDB_${PACKAGE_NAME} "${package} Compression in the RocksDB storage engine")
+  ADD_FEATURE_INFO(ROCKSDB_${package} HAVE_ROCKSDB_${package} "${package} Compression in the RocksDB storage engine")
 
-  IF(${${var}_VALID})
-    MESSAGE_ONCE(rocksdb_${var} "Found ${package}: ${${var}_LIBRARIES}")
-    add_definitions(-D${PACKAGE_NAME})
+  IF(${${package}_VALID})
+    MESSAGE_ONCE(rocksdb_${package} "Found ${package}: ${${var}_LIBRARIES}")
+    add_definitions(-D${var})
     include_directories(${${var}_INCLUDE_DIR})
     list(APPEND THIRDPARTY_LIBS ${${var}_LIBRARIES})
-  ELSEIF(${${PACKAGE_NAME}_FOUND})
-    MESSAGE_ONCE(rocksdb_${var} "Found unusable ${package}: ${${var}_LIBRARIES} [${ARGV2}]")
+  ELSEIF(${${package}_FOUND})
+    MESSAGE_ONCE(rocksdb_${package} "Found unusable ${package}: ${${var}_LIBRARIES} [${ARGV1}]")
   ELSE()
-    MESSAGE_ONCE(rocksdb_${var} "Could NOT find ${package}")
+    MESSAGE_ONCE(rocksdb_${package} "Could NOT find ${package}")
   ENDIF()
 
-  IF (${WITH_ROCKSDB_${package}} STREQUAL "ON"  AND NOT ${${PACKAGE_NAME}_FOUND})
+  IF (${WITH_ROCKSDB_${package}} STREQUAL "ON"  AND NOT ${${package}_FOUND})
     MESSAGE(FATAL_ERROR
       "${package} library was not found, but WITH_ROCKSDB_${package} option is ON.\
       Either set WITH_ROCKSDB_${package} to OFF, or make sure ${package} is installed")
   endif()
 endmacro()
 
-check_lib(LZ4    LZ4)
-check_lib(BZip2  BZIP2)
-check_lib(snappy snappy) # rocksdb/cmake/modules/Findsnappy.cmake violates the convention
-check_lib(ZSTD   ZSTD ZDICT_trainFromBuffer)
+check_lib(LZ4)
+check_lib(BZip2)
+check_lib(Snappy)
+check_lib(ZSTD ZDICT_trainFromBuffer)
 
 add_definitions(-DZLIB)
 list(APPEND THIRDPARTY_LIBS ${ZLIB_LIBRARY})