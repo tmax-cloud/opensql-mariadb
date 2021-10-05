TheGOro/mariadb-server

MDEV-25004 Review

storage/innobase/row/row0upd.cc
@@ -2580,8 +2580,10 @@ row_upd_sec_step(
 	    || row_upd_changes_ord_field_binary(node->index, node->update,
 						thr, node->row, node->ext)) {
 
-		if (node->index->vers_skip_fts_doc_id() &&
-		    node->row->vers_history_row()) {
+		if (node->table->versioned()
+		    && node->index->is_vers_fulltext()
+		    && node->row->fields[node->table->vers_end]
+			.vers_history_row()) {
 			return DB_SUCCESS;
 		}
 		return(row_upd_sec_index_entry(node, thr));

tmax-cloud/opensql-mariadb

storage/innobase/row/row0upd.cc
@@ -2580,8 +2580,9 @@ row_upd_sec_step(
 	    || row_upd_changes_ord_field_binary(node->index, node->update,
 						thr, node->row, node->ext)) {
 
-		if (node->index->vers_skip_fts_doc_id() &&
-		    node->row->vers_history_row()) {
+		if (node->index->vers_fulltext()
+		    && node->row->fields[node->table->vers_end]
+			.vers_history_row()) {
 			return DB_SUCCESS;
 		}
 		return(row_upd_sec_index_entry(node, thr));